bf0995bb5abfd18ebc29942904261032fba69be7
CameronPennington <cameron.pennington@gmail.com>
2020-02-18 14:49:30 -0500

    setup_db function added to models.py

--- models.py
+++ models.py
@@ -3,5 +3,12 @@ from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-SQLALCHEMY_DATABASE_URI = 'postgres://postgres:postgres@localhost:5432/casting'
-SQLALCHEMY_TRACK_MODIFCATIONS = True
+SQLALCHEMY_TRACK_MODIFCATIONS = False
+
+db = SQLAlchemy()
+
+def setup_db(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:postgres@localhost:5432/casting'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
