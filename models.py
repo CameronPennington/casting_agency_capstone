ddf2caedaa6303e7f9372bd7619e15831592ad7b
CameronPennington <cameron.pennington@gmail.com>
2020-02-18 14:44:28 -0500

    added db variables to models.py

--- models.py
+++ models.py
@@ -2,3 +2,6 @@ import os
 from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
 import json
+
+SQLALCHEMY_DATABASE_URI = 'postgres://postgres:postgres@localhost:5432/casting'
+SQLALCHEMY_TRACK_MODIFCATIONS = True
