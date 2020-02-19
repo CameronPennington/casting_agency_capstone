bfb9ae9ac66c540deb6d030363170aecbb6ab7dc
CameronPennington <cameron.pennington@gmail.com>
2020-02-19 07:35:39 -0500

    finished manage.py file

--- manage.py
+++ manage.py
@@ -2,13 +2,13 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from app import APP
-# from models import db
+from models import db
 
-# migrate = Migrate(app, db)
-# manager = Manager(app)
+migrate = Migrate(APP, db)
+manager = Manager(APP)
 
-# manager.add_command('db', MigrateCommand)
+manager.add_command('db', MigrateCommand)
 
 
-# if __name__ == '__main__':
-#     manager.run()
+if __name__ == '__main__':
+    manager.run()
