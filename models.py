6e029ddd857dbc3a8c2aba9b4ef2ddee00c1d1f7
CameronPennington <cameron.pennington@gmail.com>
2020-02-19 08:47:36 -0500

    added movies model and migrated successfully

--- models.py
+++ models.py
@@ -11,3 +11,8 @@ def setup_db(app):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
+
+class Movie(db.Model):
+    id = Column(db.Integer, primary_key = True)
+    title = Column(db.String(30), nullable = False)
+    release_date = Column(db.String(30), nullable = False)
