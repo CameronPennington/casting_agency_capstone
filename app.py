ab9354101e13fce31562c389c1ad6a685d6b6e7d
CameronPennington <cameron.pennington@gmail.com>
2020-02-20 10:10:21 -0500

    removed request method check from post movies route and defined variables before assigning request data to attributes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, abort, jsonify
 from sqlalchemy import exc
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from models import setup_db, Movie, Actor
+from models import db, setup_db, Movie, Actor
 from auth import requires_auth
 
 def create_app(test_config=None):
@@ -15,24 +15,24 @@ def create_app(test_config=None):
   return app
 
 
+
 APP = create_app()
-db = SQLAlchemy()
+
+
+
+# db = SQLAlchemy()
 
 @APP.route('/movies', methods=['POST'])
 @requires_auth('post:movies')
 def create_movie(token):
   try:
-    if request.method != 'GET' and request.method != 'POST':
-      abort(405)
     
     req_data = request.get_json()
-   
-    # print(req_data)
-    new_movie = Movie(
-      title = req_data['title'],
-      release_date = req_data['release_date']
-    )
-    print(new_movie)
+    title = req_data['title']
+    release_date = req_data['release_date']
+
+    new_movie = Movie( title=title, release_date = release_date)
+
     db.session.add(new_movie)
     db.session.commit()
   except Exception:
@@ -42,7 +42,7 @@ def create_movie(token):
     db.session.close()
     return jsonify({
       'success': True
-    })
+    }), 201
 
 if __name__ == '__main__':
     APP.run(host='0.0.0.0', port=8080, debug=True)
